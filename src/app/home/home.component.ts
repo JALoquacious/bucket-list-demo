0d97b1ff7926946eec0643dab49ae677bc4303ae
JALoquacious <>
2018-02-07 15:01:02 -0500

    Add animations & implement ability to remove list items

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.less']
+    styleUrls: ['./home.component.less'],
+    animations: [
+        trigger('goals', [
+            transition('* => *', [
+                query(':enter', style({ opacity: 0 }), { optional: true })
+                ,
+                query(':enter', stagger('300ms', [
+                    animate('.6s ease-in', keyframes([
+                        style({ opacity: 0.0, transform: 'translateY(-75%)', offset: 0.0 }),
+                        style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3 }),
+                        style({ opacity: 1.0, transform: 'translateY(0px)', offset: 1.0 }),
+                    ]))
+                ]), { optional: true })
+                ,
+                query(':leave', stagger('300ms', [
+                    animate('.6s ease-out', keyframes([
+                    style({ opacity: 1.0, transform: 'translateY(0px)', offset: 0.0 }),
+                    style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3 }),
+                    style({ opacity: 0.0, transform: 'translateY(-75%)', offset: 1.0 }),
+                    ]))
+                ]), { optional: true })
+            ])
+        ])
+    ]
 })
 export class HomeComponent implements OnInit {
-
-    itemCount: number = 4;
+    itemCount: number;
     btnText: string = 'Add an Item';
     goalText: string = 'My first life goal';
-    goals = [];
+    goals: string[] = ['SCUBA in the Mariana Trench', 'Climb Mount Everest and K2', 'Hibernate with polar bears'];
 
     constructor() { }
 
@@ -21,6 +44,11 @@ export class HomeComponent implements OnInit {
     addItem() {
         this.goals.push(this.goalText);
         this.goalText = '';
+        this.itemCount = this.goals.length;
+    }
+
+    removeItem(i) {
+        this.goals.splice(i, 1);
         this.itemCount = this.goals.length;
     }
 }
